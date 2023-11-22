9041be6cef6278b3f6a1a88902557b3010c907ac
unknown <adetutu.francisb@gmail.com>
2023-11-21 22:01:50 -0500

    updatejava

---
+++ app.js
@@ -0,0 +1,37 @@
+function createParagraph(){
+    var one =  document.getElementById('item1').value;
+    var two =  document.getElementById('item2').value
+    var three =document.getElementById('item3').value
+    var four = document.getElementById('item4').value
+    var five = document.getElementById('item5').value
+    var six = document.getElementById('item6').value
+    var seven = document.getElementById('item7').value
+    var eight= document.getElementById('item8').value
+    var nine= document.getElementById('item9').value
+    var ten= document.getElementById('item10').value
+   var eleven=document.getElementById('item11').value
+   var twelve= document.getElementById('item12').value
+    var thirteen=document.getElementById('item13').value
+    var fourteen = document.getElementById('item14').value
+    var fifteen =  document.getElementById('item15').value
+    var sixteen =  document.getElementById('item16').value
+    var seventeen = document.getElementById('item17').value
+    var eighteen = document.getElementById('item18').value
+    var nineteen =  document.getElementById('item19').value
+    var twenty = document.getElementById('item20').value
+    var twentyOne =document.getElementById('item21').value 
+    var twentyTwo = document.getElementById('item22').value
+    var twentyThree= document.getElementById('item23').value
+    var twentyFour = document.getElementById('item24').value
+    var twentyFive = document.getElementById('item25').value
+    var twentySix=  document.getElementById('item26').value
+    var twentySeven=document.getElementById('item27').value
+    var twentyEight= document.getElementById('item28').value
+    var twentyNine= document.getElementById('item29').value
+    var thirty= document.getElementById('item30').value
+    
+ var paragraph =  one + ' of internet' + two +  ' of about ' + three +' million ' + four +' said they went to ' + five +' to get news or '+ six +' about the ' + seven +' elections. We call them the ' + eight +' politician' + nine +' consumers. ' + ten +' of ' + eleven +' users or about ' + twelve +'million people said they used ' + thirteen +' to discuss ' + fourteen +' and one of the most ' + fifteen +' a ' + sixteen + ' was jokes about the'+ seventeen +'and the election. '+ eighteen +' of' + nineteen +' or more than '+ twenty +' million people '+ twentyOne + 'online to '+ twentyTwo + 'directly in  ' + twentyThree +' activities such as donating ' + twentyFour +', volunteering or gerund about ' + twentyFive +' events to attend. I think President ' + twentySix +' is a '+ twentySeven +'' + twentyEight +'and should be ' + twentyNine +' past tense in the ' + thirty +'.'
+
+ 
+}
+document.getElementById().innerHTML = paragraph
